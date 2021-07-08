b092ee98c9e57d6f4dfa92d4bf4abd038004c1e7
khooyenni <73241253+khooyenni@users.noreply.github.com>
2021-07-08 10:42:50 +0800

    Add files via upload

---
+++ Produce/Discount.java
@@ -0,0 +1,6 @@
+package Produce;
+
+public interface Discount {
+	double discountRate();
+}
+
---
+++ Produce/PaymentDiscount.java
@@ -0,0 +1,14 @@
+package Produce;
+
+class Discount1 implements Discount {
+	public double discountRate() {
+		return 0.1;
+	}
+}
+
+class Discount2 implements Discount{
+	public double discountRate() {
+		return 0.2;
+	}
+}
+
